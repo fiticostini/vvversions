216691b8d4855d52c7ea387633a3dae8dbbb26b7
fiticostini <fiticostini@gmail.com>
2023-04-27 17:04:03 +0000

    se anadio clase user

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
 
+class Role(enum.Enum):
+    admin = "admin"
+    user = "user"
+
+class Banda(enum.Enum):
+    solista = "solista"
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(240), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    role = db.Column(db.Enum(Role), nullable=False, default="user")
+    banda = de.Column(db.Enum(Banda), nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
+            "fullname": self.fullname,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_active": self.is_active,
+            "role": self.role,
+            "banda": self.banda
         }
