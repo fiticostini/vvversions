0a43f71dda8c76cb2c2a53e8c21dfa49d7b0b445
bostonrex <reynaldogoitia@gmail.com>
2023-04-29 17:38:26 +0000

    primer merge navbar, register, formulario

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,16 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 
 export const Navbar = () => {
+	const location = useLocation()
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className={location.pathname==`/register`? `navbar navbarregister` : location.pathname==`/single`? `navbar navbarsingle`: `navbar navbarhome` }>
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">VVVERSIONS</span>
 				</Link>
 				<div className="ml-auto">
+					<Link to="/register">
+						<button className="botonregister text-white me-3">Register</button>
+					</Link>
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="botonlogin text-dark">Sign In</button>
 					</Link>
 				</div>
 			</div>
---
+++ src/front/js/component/publicity.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+let phrases = ["A place where you can follow the growth of your songs, enough of songs by whatsapp or emails that are lost in nowhere, VVVersions has arrived",
+    "a single payment a year, you can include your entire band, your manager and even your best fans",
+    "Can you imagine having saved all the versions of your songs? incredible content, worthless", "one of the best music apps of the year"]
+
+
+const Publicity = () => {
+    return (
+<div>
+
+</div>
+    )
+};
+
+
+export default Publicity;
+
+
