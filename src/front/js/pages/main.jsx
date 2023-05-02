0cbeced0e38c2d2f8b56ee0ffe7c1721ccf814b6
fiticostini <fiticostini@gmail.com>
2023-05-02 01:55:19 +0000

    cambios de main.jsx

--- src/front/js/pages/main.jsx
+++ src/front/js/pages/main.jsx
@@ -9,8 +9,7 @@ return (
     <div> 
         <h1>{store.artistName}</h1>
         <div className="container sticky-top">
-        <div className="col-8 bg-warning"> hola
-        </div>
+        <div className="col-8 bg-warning"> hola</div>
         <div className="col-8 bg-dark">hola2</div>
         <div className="col-8 bg-danger">hola3</div>
         <div className="col-8 bg-success">hola3</div>
