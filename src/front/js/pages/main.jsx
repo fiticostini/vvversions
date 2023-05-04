dd961792e099964c4ef686411cf481520ae2906e
NikoDemo96 <chnicolas.aranguiz@gmail.com>
2023-05-04 16:54:31 +0000

    reproductor listo

--- src/front/js/pages/main.jsx
+++ src/front/js/pages/main.jsx
@@ -1,28 +1,20 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-
+import MusicPlayer from "../component/MusicPlayer.jsx"
 
 
 export const Main = () => {
-    const {store} = useContext(Context)
-return (
-    <div> 
-
-        <h1>{store.artistName}</h1>
-        <div className="container sticky-top">
-        
-
-        <div className="col-8 bg-warning"> hola
-        </div>
+  const { store } = useContext(Context);
+  return (
+    <div>
+      <MusicPlayer/>
+      <h1>{store.artistName}</h1>
+      <div className="container sticky-top">
+        <div className="col-8 bg-warning"> hola</div>
         <div className="col-8 bg-dark">hola2</div>
         <div className="col-8 bg-danger">hola3</div>
         <div className="col-8 bg-success">hola3</div>
-        </div>
-        
-
-        
+      </div>
     </div>
-    
-)
-
-} 
+  );
+};
