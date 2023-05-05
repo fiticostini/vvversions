fde6af73fb4c4f336770b25a147c018e599c4472
bostonrex <reynaldogoitia@gmail.com>
2023-05-05 03:35:32 +0000

    componente add project agregado

--- src/front/js/pages/main.jsx
+++ src/front/js/pages/main.jsx
@@ -6,14 +6,11 @@ import ProyectRevision from "../component/ProyectRevision.jsx";
 export const Main = () => {
   const { store } = useContext(Context);
   return (
-    <div>
+    <div className="p-5">
       <ProyectRevision />
       <h1>{store.artistName}</h1>
       <div className="container sticky-top">
-        <div className="col-8 bg-warning"> hola</div>
-        <div className="col-8 bg-dark">hola2</div>
-        <div className="col-8 bg-danger">hola3</div>
-        <div className="col-8 bg-success">hola3</div>
+        
       </div>
     </div>
   );
